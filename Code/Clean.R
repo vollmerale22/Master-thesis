cbf5f0fc45c1cd2bc632f4afb5b420a681e9334e
vollmerale22 <vollmeralessandra@yahoo.de>
2024-11-30 17:26:54 +0100

    Code

---
+++ Code/Clean.R
@@ -0,0 +1,73 @@
+doClean <- function(data_init,min_start_date){
+  
+  # Inputs:
+  # data_init [data.frame] = initial data-set read from Stata
+  # min_start_date [string] = minimum start date for variables
+  #                           NB: should be in "YYYY-MM-15" format (pay attention to put "15" for the day)
+  # Output: 
+  # data_rmv = cleaned data-set 
+  
+  # -----------------------------------
+  # 1 - Remove variables starting after the date set by the user
+  # -----------------------------------
+  
+  data_rmv <- data_init
+  
+  # Remove variables starting after min_start_date
+  ctrl_min <- data_rmv %>%
+    filter(date==as.Date(min_start_date)) %>%
+    reshape2::melt(id.vars = "date", 
+                   variable.name = "variable",
+                   value.name = "value") %>%
+    filter(!is.na(value)) %>%
+    mutate(variable=as.character(variable))
+  
+  var_to_keep <- c(unique(ctrl_min$variable),"date")
+  
+  data_rmv %<>%
+    select(all_of(var_to_keep))
+  
+  
+  # -----------------------------------
+  # 2 - Clean data (interpolate missing observations and remove non-stationary variables)
+  # -----------------------------------
+  
+  # Interpolate data missing between two points (but not leading or trailing NAs)
+  data_rmv %<>%
+    mutate(across(-date, ~na.approx(.,na.rm = FALSE)))
+  
+  # Remove non-stationary variables - by ADF test, removing if p-value > 10%
+  temp <- data_rmv %>%
+    select(-date,
+           -target)
+  var_nonstat <- c()
+  
+  for (jj in 1:ncol(temp)){
+    
+    # Select individual variable and drop NAs
+    temp_col <- temp[jj]
+    temp_col <- drop_na(as.data.frame(temp_col))
+    temp_name <- colnames(temp_col)
+    
+    # Run ADF test and add variable to removal list if p-value > 10%
+    temp_adf <- adf.test(temp_col[,1])
+    if(temp_adf$p.value > 0.10){
+      var_nonstat <- c(var_nonstat,temp_name)
+    }
+  }
+  
+  data_rmv %<>%
+    select(-all_of(var_nonstat))
+  
+  
+  # -----------------------------------
+  # 3 - Return data-set
+  # -----------------------------------
+  
+  return(data_rmv)
+  
+}
+
+
+x <- arima.sim(list(order = c(1,1,0),ar = 0.2),n = 100)
+adf.test(x)
---
+++ Code/Main.R
@@ -0,0 +1,401 @@
+# Load packages
+library(data.table)
+library(purrr)
+library(tidyverse)
+library(readr)
+library(lars)
+library(readxl)
+library(writexl)
+library(haven)
+library(magrittr)
+library(haven)
+library(lubridate)
+library(FactoMineR)
+library(factoextra)
+library(randomForest)
+library(glmnet)
+library(xgboost)
+library(keras)
+library(tensorflow)
+library(quantreg)
+library(MSwM)
+library(BMA)
+library(zoo)
+library(tseries)
+library(doBy)
+library(MacroRF)
+library(tidyr)
+library(ggthemes)
+library(devtools)
+library(sandwich)
+library(lmtest)
+library(xtable)
+library(stargazer)
+library(dplyr)
+library(scales)
+library(data.table)
+
+
+rm(list = ls())
+# Function to check stationarity using the Augmented Dickey-Fuller (ADF) test
+is_stationary <- function(series, significance_level = 0.05) {
+  # Ensure no NA values are present
+  series <- na.omit(series)
+  adf_test <- adf.test(series)
+  return(adf_test$p.value <= significance_level)  # Check if p-value <= significance level
+}
+# Function to standardise a column
+standardise <- function(series) {
+  return((series - mean(series, na.rm = TRUE)) / sd(series, na.rm = TRUE))
+}
+
+# Load the Excel sheet
+data <- read_excel("EU DATA.xlsx")
+
+# Data transformation: check stationarity and apply transformations
+transformed_data <- data %>%
+  mutate(across(where(is.numeric), ~ {
+    if (!is_stationary(.)) {
+      print(paste(cur_column(), "is non-stationary. Applying differencing and standardising."))
+      return(standardise(c(NA, diff(.))))  # Differencing and standardising
+    } else {
+      print(paste(cur_column(), "is stationary. Standardising directly."))
+      return(standardise(.))  # Standardising directly
+    }
+  }))
+# Save the transformed data to a new Excel file
+output_file <- "transformed_data.xlsx"  # Replace with your desired output file name
+write_xlsx(transformed_data, output_file)
+
+# ----------------------------------------------------------------------------
+# STEP 0 - SET USER PARAMETERS FOR THE HORSERACE
+# ----------------------------------------------------------------------------
+name_input <- "transformed_data.xlsx"  # Name of the input file
+min_start_date <- "2001-01-15"          # Minimum start date for variables (otherwise discarded)
+start_date_oos <- "2024-07-15"          # Start date for OOS predictions
+end_date_oos <- "2024-07-15"            # End date for OOS predictions
+
+list_h <- c(-1, 0, 1)                      # List of horizons for back-, now- or fore-cast takes place
+# Negative for a back-cast, 0 for a now-cast and positive for a fore-cast
+list_methods <- c( 3, 4)                  # List of pre-selection methods
+# 0 = No pre-selection
+# 1 = LARS (Efron et al., 2004)
+# 2 = Correlation-based (SIS: Fan and Lv, 2008)
+# 3 = t-stat based (Bair et al., 2006)
+# 4 = Iterated Bayesian Model Averaging (BMA: Yeung et al., 2005)
+list_n <- c(40,60)                      # List of number of variables kept after pre-selection
+list_reg <- c(1,2,3,4,5,6,7)                      # List of regressions techniques
+# The AR benchmark is always performed - regardless of selection
+# 1 = OLS
+# 2 = Markov-switching regression [requires 1]
+# 3 = Quantile regression
+# 4 = Random forest
+# 5 = XG Boost tree
+# 6 = Macroeconomic Random Forest
+# 7 = XG Boost linear
+
+# User parameters - 3
+# Optional inputs (calibration)
+do_factors <- 1                         # Switch on whether to do factors or not
+# 0 = no factors
+# 1 = factors
+fast_bma <- 1                           # 1 = fast version - i.e. runs with less iterations
+# 0 = full number of iterations
+n_per <- 12                             # Number of periods (last available ones) on which the optimization of the hyper-parameters for ML is performed
+sc_ml <- 1                              # Switch on whether the data should be scaled for ML methods (0 = no, 1 = yes)
+fast_MRF <- 1                           # 1 = fast tuning only on number of variables in linear part
+# 0 = full tuning on 5 hyper-parameters (see code)
+
+
+# Load functions
+source("Code/Clean.R")         # Program cleaning the dataset (interpolation of missing data, elimination of variables starting after min_start_date)
+source("Code/Align.R")         # Program doing the re-alignment - see section 2.1 of Chinn et al. (2023)
+source("Code/PreSelection.R")  # Program doing the pre-selection - see section 1.3 of Chinn et al. (2023)
+source("Code/Regression.R")    # Program doing the regressions - see section 1.5 of Chinn et al. (2023)
+source("COde/Tuning.R")        # Program doing the tuning of hyper-parameters for machine learning methods
+
+
+# ----------------------------------------------------------------------------
+# STEP 2 - READ (AND CLEAN) THE USER-PROVIDED DATASET
+# ----------------------------------------------------------------------------
+
+# Read data
+data_init <- read_excel(paste0(name_input)) %>%
+  mutate(date=ymd(date))
+
+# Clean data-set
+data_rmv <- doClean(data_init,min_start_date)
+
+
+# ----------------------------------------------------------------------------
+# STEP 3 - PERFORM THE HORSERACE (LOOP OVER USER PARAMETERS)
+# ----------------------------------------------------------------------------
+
+# Loop over user-defined horizons
+for (hh in 1:length(list_h)){
+  
+  # Get horizon to test
+  horizon = as.numeric(list_h[hh])
+  
+  # Get re-aligned dataset
+  data_real <- doAlign(data_rmv,horizon)
+  
+  # Initializing the output
+  summary_ps_meth <- data.frame(NA)
+  
+  # Loop over user-defined methods
+  for (mm in 1:length(list_methods)){
+    
+    # Initializing the output of the results comparison exercise
+    select_method = as.numeric(list_methods[mm])
+    
+    # Loop over user-defined number of variables
+    for (nn in 1:length(list_n)){
+      
+      # Get values of the test
+      n_var = as.numeric(list_n[nn])
+      
+      # Print
+      print('======================================================================================================')
+      print(paste0("HORIZON = ",horizon))
+      print(paste0("METHOD = ",select_method))
+      print(paste0("NUMBER OF VARIABLES = ",n_var))
+      print('======================================================================================================')
+      
+      # Determine starting and ending dates for out-of-sample exercise
+      n_start <- which(grepl(start_date_oos, as.character(data_real$date)))
+      n_end <- which(grepl(end_date_oos, as.character(data_real$date)))
+      
+      # Prepare results dataset
+      results <- data.frame(matrix(NA,
+                                   nrow = n_end - n_start +1,
+                                   ncol = length(list_reg)+3))
+      
+      # Loop over dates for out-of-sample predictions
+      for (ii in n_start:n_end){
+        
+        # Data and dates
+        data_all <- head(data_real,ii)
+        date_ii <- data_all$date[ii]
+        year <- substr(date_ii,1,4)
+        month <- substr(date_ii,6,7)
+        
+        # Print
+        print(paste0("Doing out-of-sample predictions for ",year," at month ",month))
+        
+        
+        # --------------------------------------------------------------------
+        # STEP A: Pre-selection
+        # --------------------------------------------------------------------
+        
+        # Run pre-selection function
+        var_sel <- pre_select(data_real,ii,horizon,select_method,n_var)
+        
+        # Check that the number of variables is correct
+        if(length(var_sel)!=n_var && select_method != 2){
+          stop("Pre-selection step did not work, please check")  
+        }
+        
+        # Get LHS and RHS datasets with only the variables from the pre-selection
+        rhs_sel <- data_all %>%
+          select(date,all_of(var_sel))
+        
+        lhs_sel <- data_all %>%
+          select(date,
+                 target,
+                 L1st_target,
+                 L2nd_target)
+        
+        # Cleaning
+        rm(var_sel)
+        
+        # --------------------------------------------------------------------
+        # STEP B: Factor extraction (PCA) on pre-selected variables
+        # --------------------------------------------------------------------
+        
+        # Clean NAs in the RHS data
+        x_pca <- rhs_sel %>%
+          drop_na()
+        
+        # Clean the NAs also in LHS 
+        start_date <- as.Date(x_pca$date[1])
+        end_date <- as.Date(x_pca$date[nrow(x_pca)])
+        
+        lhs_sel %<>%
+          filter(date>=start_date) %>%
+          filter(date<=end_date)
+        
+        if(do_factors==1){
+          
+          # Run PCA
+          x_pca %<>% select(-date)
+          
+          res_pca <- PCA(X = x_pca,
+                         scale = TRUE,
+                         graph = FALSE,
+                         ncp = length(x_pca))
+          
+          rhs_fct <- res_pca$ind$coord %>%
+            as.data.table()
+          
+          # Keep only the factors with eigen values > 1 = rule of the thumb of Kaiser (1961)
+          # If eigen value > 1 then explain more variance than single variable
+          n_fct <- dim(rhs_fct)[2]
+          rhs_fct_sel <- rhs_fct %>%
+            select(1:all_of(n_fct))
+          
+          # Create final dataset for regression
+          don_cb <- cbind(lhs_sel, rhs_fct_sel)
+          
+          # Cleaning
+          rm(eig_val)
+          rm(res_pca)
+          rm(rhs_fct)
+          rm(rhs_fct_sel)
+          rm(n_fct)
+          
+        }else{
+          
+          # Create alternative dataset without factors and CSV output
+          don_cb <- cbind(lhs_sel,select(x_pca,-date))
+          
+        }
+        
+        # Cleaning
+        rm(rhs_sel)
+        rm(end_date)
+        rm(start_date)
+        rm(lhs_sel)
+        rm(x_pca)
+        
+        
+        # --------------------------------------------------------------------
+        # STEP C: Regression on factors
+        # --------------------------------------------------------------------
+        
+        # Get current date
+        don_reg <- don_cb %>%
+          drop_na()
+        n_date_ii <- which(grepl(date_ii, as.character(don_reg$date)))
+        
+        # Define datasets in- and out-of-sample
+        smpl_in <- head(don_reg,n_date_ii-horizon-3)
+        smpl_out <- dplyr::slice(don_reg,n_date_ii)
+        results[ii-n_start+1,1] <- date_ii
+        
+        # Run regressions
+        temp_res <- run_regressions(smpl_in,
+                                    smpl_out,
+                                    list_reg,
+                                    n_sel,
+                                    sc_ml,
+                                    fast_MRF)
+        
+        # Write results  
+        results[ii-n_start+1,2:length(results)] <- temp_res
+        colnames(results) <- c('date',colnames(temp_res))
+        
+      }
+      
+      # Create mean of predictions (excluding AR)
+      results %<>%
+        mutate(pred_mean = rowMeans(select(results,starts_with("pred_"))),
+               date = as_date(as.Date(as.POSIXct(date*24*60*60, origin="1970-01-01"))),
+               year = year(date))
+      
+      # Create directory
+      dir.create(file.path(""), showWarnings = FALSE)
+      dir.create(file.path(paste0("",horizon)), showWarnings = FALSE)
+      
+      # Write results (predictions)
+      write.csv(select(results,-year), file = paste0("",
+                                                     paste0("h",horizon,"/"),
+                                                     "pred_sel_",
+                                                     select_method,
+                                                     "_n_",
+                                                     n_var,
+                                                     "_reg_",
+                                                     paste(list_reg, collapse='_'),
+                                                     "_h_",
+                                                     horizon,
+                                                     "_",
+                                                     start_date_oos,
+                                                     "_",
+                                                     end_date_oos,
+                                                     ".csv"),
+                row.names=FALSE)
+      
+      # Summarizing and writing aggregate results
+      err <- function(X,Y){sqrt(sum((X-Y)^2)/length(Y))}
+      
+      total <- results %>%
+        select(-date,-year) %>%
+        apply(2,err,Y=results[,2])
+      
+      crisis <- results %>%
+        filter((year %in% c(2008,2009,2020,2021))) %>%
+        select(-date,-year)
+      
+      crisis <- apply(crisis,2,err,Y=crisis[,1])
+      
+      normal <- results %>%
+        filter(!(year %in% c(2008,2009,2020,2021))) %>%
+        select(-date,-year)
+      
+      normal <- apply(normal,2,err,Y=normal[,1])
+      
+      summary_all <- cbind(cbind(total,crisis),normal) %>%
+        as.data.frame()
+      summary_all <- summary_all[!(row.names(summary_all) %in% c('true_value')),]
+      
+      # Writing results
+      # Results are written for RMSE on crisis sample (2008-2009 and 2020-2021) and non-crisis sample
+      write.csv(summary_all, file = paste0("",
+                                           paste0("h",horizon,"/"),
+                                           "rmse_sel_",
+                                           select_method,
+                                           "_n_",
+                                           n_var,
+                                           "_reg_",
+                                           paste(list_reg, collapse='_'),
+                                           "_h_",
+                                           horizon,
+                                           "_",
+                                           start_date_oos,
+                                           "_",
+                                           end_date_oos,
+                                           ".csv"))
+      
+      # Write results to the summary
+      tot_meth <- length(list_methods)
+      num_col <- 1+(mm-1)*tot_meth+nn
+      summary_ps_meth[1,num_col] <- select_method
+      summary_ps_meth[2,num_col] <- n_var
+      summary_ps_meth[3:(nrow(summary_all)+2),num_col] <- summary_all$total
+      summary_ps_meth[3:(nrow(summary_all)+2),1] <- rownames(summary_all)
+      
+    } # End of loop on number of variables
+  } # End of loop on pre-selection method
+  
+  # Write results
+  # This is a summary of all the tested methods
+  summary_ps_meth[1,1] <- "pre-selection"
+  summary_ps_meth[2,1] <- "nb variables"
+  write.csv(summary_ps_meth, file = paste0("",
+                                           paste0("h",horizon,"/"),
+                                           "summaryALL_sel_",
+                                           paste(list_methods,collapse='_'),
+                                           "_n_",
+                                           paste(list_n,collapse='_'),
+                                           "_reg_",
+                                           paste(list_reg, collapse='_'),
+                                           "_h_",
+                                           horizon,
+                                           "_",
+                                           start_date_oos,
+                                           "_",
+                                           end_date_oos,
+                                           ".csv"),
+            row.names=FALSE)
+  
+} # End of loop on horizon
